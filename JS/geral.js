2a7d37551c667be397e8aec12d7ec587257203bc
Alê <xavieralessandrosantos@gmail.com>
2022-04-14 00:04:54 -0300

    Adicionando alguns comentarios para facilitar o estudo

--- JS/geral.js
+++ JS/geral.js
@@ -1,21 +1,26 @@
 //VARIAVEL PARA PUXAR O ELEMENTO CANVAS DO HTML PARA O JS
 const canvas = document.querySelector('canvas');
+
+//VARIAVEL QUE "MANIPULO" A DIMENSÃO DO JOGO
 const c = canvas.getContext('2d');
 
+//SETANDO A ALTURA E A LARGURA DO CANVAS
 canvas.width = 1024;
 canvas.height = 576;
 
+//PESQUISAR MAIS A FUNDO PARA VÊ PARA QUE SERVE ESSA FUNÇÃO "fillRect"
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+//CRIANDO UMA CLASSE PARA CONSTRUIR O JOGADOR
 class Sprite{
-    constructor(position)
+    constructor(position)//POSIÇÃO ONDE O PLAYER VAI ESTAR LOCALIZADO
     {
         this.position = position
     }
     draw()
     {
-       c.fillStyle = 'red' 
-       c.fillRect(this.position.x, this.position.y, 50, 150) 
+       c.fillStyle = 'red'//COR DO PLAYER 
+       c.fillRect(this.position.x, this.position.y, 50, 150)//SETANDO A POSIÇÃO ONDE O PLAYER ESTÁ LOCALIZADO 
     }
 }
 
@@ -25,5 +30,6 @@ const player = new Sprite({
     y: 0
 })
 
+//CHAMANDO CLASS COM O METODO "draw"
 player.draw();
-console.log(player);
+console.log("POSIÇÃO DO JOGADOR/PLAYER",player);
